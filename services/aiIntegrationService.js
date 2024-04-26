498a28e6da47b979b5dfecdd430983a5b9b7a6c5
RichardNeighbour <32851258+RichardNeighbour@users.noreply.github.com>
2024-04-26 20:13:57 +0100

    Fixed OpenAI API usage and import statements

--- services/aiIntegrationService.js
+++ services/aiIntegrationService.js
@@ -1,10 +1,10 @@
-import { OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from "openai";
 import logger from "../utils/logger.js";
 
-
-const openai = new OpenAIApi({
+const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
+const openai = new OpenAIApi(configuration);
 
 async function generateStoryline(promptText) {
   try {
